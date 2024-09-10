7ccddc01a0c172179bb558a21986b843c54db45c
Vadim Mitko <vadimmitko@Vadims-MacBook-Pro.local>
2024-09-10 22:38:37 +0300

    added new animation to cross in the burger menu

--- src/app/_components/BurgerMenu.tsx
+++ src/app/_components/BurgerMenu.tsx
@@ -30,30 +30,33 @@ const BurgerMenu = ({
       className="z-10 relative h-[30px] w-[30px] overflow-hidden  bg-white"
     >
       <span
-        className={` absolute top-0 right-0 left-0 bottom-0 transition ease-in-out duration-150 origin-top-left ${
-          isOpenBurger ? "" : "-rotate-90"
+        className={` absolute top-0 right-0 left-0 bottom-0 transition ease-in-out duration-150 ${
+          isOpenBurger ? "" : "[transform:rotateX(90deg)]"
         }`}
       >
         <Plus height="30" width="30" transform="rotate(45)" />
       </span>
+      <div>
+        <span
+          className={` absolute top-0  right-1 left-0 bottom-0 transition ease-in-out  origin-right ${
+            isHovered && !isOpenBurger
+              ? "duration-500"
+              : `duration-500 [transform:rotate${
+                  isOpenBurger ? "X" : "Y"
+                }(90deg)]`
+          } ${isOpenBurger ? "duration-75" : ""}`}
+        >
+          <Kanban height="30" width="30" transform="rotate(-90) scale(1,-1)" />
+        </span>
 
-      <span
-        className={` absolute top-0  right-1 left-0 bottom-0 transition ease-in-out  origin-right ${
-          isHovered && !isOpenBurger
-            ? "duration-500"
-            : "duration-500 [transform:rotateY(90deg)]"
-        } ${isOpenBurger ? "duration-75" : ""}`}
-      >
-        <Kanban height="30" width="30" transform="rotate(-90) scale(1,-1)" />
-      </span>
-
-      <span
-        className={`absolute duration-500 top-0 right-2 left-0 bottom-0 ease-in-out  origin-right  ${
-          isHovered || isOpenBurger ? "[transform:rotateY(90deg)]" : ""
-        }`}
-      >
-        <ChartNoAxesColumn height="30" width="30" transform="rotate(-90)" />
-      </span>
+        <span
+          className={`absolute duration-500 top-0 right-2 left-0 bottom-0 ease-in-out  origin-right  ${
+            isHovered || isOpenBurger ? "[transform:rotateY(90deg)]" : ""
+          }`}
+        >
+          <ChartNoAxesColumn height="30" width="30" transform="rotate(-90)" />
+        </span>
+      </div>
     </button>
   );
 };
